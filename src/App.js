f66da50626dd44fa9a25a58f6ef874153487a6ab
Jean-Pierre Gazian <jeanpierregazian@msn.com>
2022-02-08 14:08:20 +0000

    adding Hip-Hop

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     {name: "All", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/json"},
     {name: "Rock", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=21/json"},
     {name: "Dance", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=17/json"},
-    {name: "Country", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=6/json"}
+    {name: "Country", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=6/json"},
+    {name: "Hip-Hop/Rap", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=18/json"}
   ]
   
   return (
